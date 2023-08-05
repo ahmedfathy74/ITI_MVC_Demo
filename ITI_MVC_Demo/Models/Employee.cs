3f0092563cb3734eb074b68a165d3959abab889c
ahmed2016170053 <ahmed2016170053@cis.asu.edu.eg>
2023-08-05 13:41:54 +0300

    Edit Employee class

--- ITI_MVC_Demo/Models/Employee.cs
+++ ITI_MVC_Demo/Models/Employee.cs
@@ -1,12 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ITI_MVC_Demo.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Name Required ")]
+        [MaxLength(25)]
+        [MinLength(3)]
         public string Name { get; set; } = null!;
-        public int Salary { get; set; } 
+        public int Salary { get; set; }
+        [RegularExpression(@"w\{1,}\.(jpg|png)",ErrorMessage ="image must be jpj or png")]
         public string? Address { get; set; }
         public string Image { get; set; } = null!;
         [ForeignKey("department")] // we decalre here that the Dept_id is a foreign key based on ref obj from Department
